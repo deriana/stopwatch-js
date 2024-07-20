bb67f5fbf4e7a8f18da49f73ed7eb06cb1739375
deryana <derana765@gmail.com>
2024-07-21 02:16:41 +0700

    waktu adalah waktu

---
+++ script.js
@@ -0,0 +1,51 @@
+let [miliseconds,seconds, minutes, hours] = [0,0,0,0]
+
+const timeRef = document.querySelector(".timer")
+let int = null
+
+document.getElementById("start").addEventListener("click", () => {
+    if (int !== null) {
+        clearInterval(int)
+    }
+    int = setInterval(displayTimer, 10)
+})
+
+document.getElementById("stop").addEventListener("click", () => {
+    clearInterval(int)
+})
+
+document.getElementById("reset").addEventListener("click", () => {
+    clearInterval(int);
+    [miliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+    timeRef.innerHTML = ("00:00:00:000")
+});
+
+
+
+function displayTimer() {
+    miliseconds += 10
+    if (miliseconds == 1000) {
+        miliseconds = 0
+        seconds++
+        if (seconds == 60) {
+            seconds = 0
+            minutes++
+            if (minutes == 60) {
+                minutes = 0
+                hours++
+            }
+        }
+    }
+
+    let h = hours < 10 ? "0" + hours : hours
+    let m = minutes < 10 ? "0" + minutes : minutes
+    let s = seconds < 10 ? "0" + seconds : seconds
+    let ms =
+        miliseconds < 10
+        ? "00" + miliseconds
+        : miliseconds < 100
+        ? "0" + miliseconds
+        :miliseconds
+
+    timeRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`
+}
